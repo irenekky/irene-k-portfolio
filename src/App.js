5cb4fe07ba31c35eada2d6810d9970ba78417b35
Irene Kilonzo <irenedunge@yahoo.com>
2020-09-08 09:38:13 +0300

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './W3.css';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <React.Fragment>
+       <h1 className="">
+         My C.v/Resume</h1>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h3>Irene Kilonzo</h3>
+              <h5>Front-end developer</h5>
+
+Creative, visionary and responsible frontend developer with one and a half years’ experience in the IT domain. Self-taught and extremely knowledgeable of various web development technologies. Fast learner and flexible.
+
+<h3>TECHNICAL SKILLS</h3>
+<ul>
+  <li>
+React.js      Create-react-app, R.T.L, jest, custom react set-ups, react-redux</li>
+
+
+
+
+
+<li>Strong experience in working with CSS Backgrounds, CSS Layouts, CSS Positioning, CSS Animations, CSS Text, CSS Borders, CSS Margin, CSS Padding, CSS Table and Pseudo Classes.
+</li>
+
+ <li>Experience on working with CSS Backgrounds, CSS Layouts, CSS Positioning, CSS Animations, CSS Text, CSS Borders, CSS Margin, CSS Padding, CSS Table and Pseudo Classes.</li>
+
+<li>Expertise in Client Side designing and validations using HTML, JSX and Java Script.</li>
+
+<li>Thorough researcher and learner.</li>
+</ul>
+
+<h3>EXPERIENCE</h3>
+<ul>
+  <li>2018-2020 Front-end development;
+
+
+Using react.JS, JavaScript, HTML, and CSS for developing single page application and achieving two-way data binding between HTML and JavaScript objects.
+
+Using JavaScript, Dom manipulation and JavaScript event to generate the data result in UI.
+
+API integrations and implementation in the front end using axios and fetch</li>
+
+
+<li>2006-2018 Independent music producer
+
+Planning and executing digital music production projects. I also did singing and songwriting independently.</li>
+</ul>
+
+
+<h3>EDUCATION</h3>
+
+
+
+Music certificate: Real music school
+
+</React.Fragment>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./W3.css";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import App from "./App";
+import Users from "./users";
+import Notfound from "./notfound";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const routing = (
+
+  
+  
+  <Router>
+  
+  
+    <div className="">
+      <React.Fragment>
+    
+      <h1 className="w3-bar-item w3-black w3-center">Irene Kilonzo</h1>
+      <p className="w3-center w3-text-pink">Front-end developer</p>
+      
+      <ul className="w3-bar w3-grey">
+        <li className="w3-bar-item w3-left w3-button w3-pink">
+          <NavLink exact activeClassName="active" to="/">
+            <h6 className="w3-text-black">About me</h6>
+          </NavLink>
+        </li>
+        <li className="w3-bar-item w3-left w3-button w3-black">
+          <NavLink activeClassName="active" to="/users">
+            <h6 className="w3-text-white">Portfolio</h6>
+          </NavLink>
+        </li>
+        
+      </ul>
+      
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        
+        <Route component={Notfound} />
+      </Switch>
+      </React.Fragment>
+      </div>
+      
+       
+      </Router>
+
+      
+    
+
+
+    
+  
+  
+  
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById("root"));
