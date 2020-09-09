74b8474b1eb46149e6d027c1cc7f176c66a22e04
Irene Kilonzo <irenedunge@yahoo.com>
2020-09-09 14:38:01 +0300

    Changes made to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './W3.css';
-class App extends React.Component {
+class About extends React.Component {
   render() {
     return (
       <div>
@@ -12,18 +12,18 @@ class App extends React.Component {
         <h3>Irene Kilonzo</h3>
               <h5>Front-end developer</h5>
 
-Creative, visionary and responsible frontend developer with one and a half years’ experience in the IT domain. Self-taught and extremely knowledgeable of various web development technologies. Fast learner and flexible.
+Creative, visionary and responsible frontend developer with five and a half years’ experience in the IT domain. Self-taught and extremely knowledgeable of various web development technologies. Fast learner and flexible.
 
 <h3>TECHNICAL SKILLS</h3>
 <ul>
   <li>
-React.js      Create-react-app, R.T.L, jest, custom react set-ups, react-redux</li>
+  Create-react-app, R.T.L, jest, custom react set-ups, react-redux, github, API intergration</li>
 
 
 
 
 
-<li>Strong experience in working with CSS Backgrounds, CSS Layouts, CSS Positioning, CSS Animations, CSS Text, CSS Borders, CSS Margin, CSS Padding, CSS Table and Pseudo Classes.
+<li>Strong experience in working with w3-CSS Backgrounds, w3-CSS Layouts, w3-CSS Positioning, w3-CSS Animations, w3-CSS Text, w3-CSS Borders, w3-CSS Margin, w3-CSS Padding, w3-CSS Table and Pseudo Classes.
 </li>
 
  <li>Experience on working with CSS Backgrounds, CSS Layouts, CSS Positioning, CSS Animations, CSS Text, CSS Borders, CSS Margin, CSS Padding, CSS Table and Pseudo Classes.</li>
@@ -35,7 +35,7 @@ React.js      Create-react-app, R.T.L, jest, custom react set-ups, react-redux</
 
 <h3>EXPERIENCE</h3>
 <ul>
-  <li>2018-2020 Front-end development;
+  <li>2018-present Front-end development;
 
 
 Using react.JS, JavaScript, HTML, and CSS for developing single page application and achieving two-way data binding between HTML and JavaScript objects.
@@ -45,7 +45,13 @@ Using JavaScript, Dom manipulation and JavaScript event to generate the data res
 API integrations and implementation in the front end using axios and fetch</li>
 
 
-<li>2006-2018 Independent music producer
+<li>2013-2018 Muthusi creatives
+
+Helped build client websites with wordpress</li>
+
+
+
+<li>2006-2012 Independent music producer
 
 Planning and executing digital music production projects. I also did singing and songwriting independently.</li>
 </ul>
@@ -53,9 +59,9 @@ Planning and executing digital music production projects. I also did singing and
 
 <h3>EDUCATION</h3>
 
-
-
-Music certificate: Real music school
+      Music certificate: Real music school
+      HighSchool certificate :Greenfields high school
+     
 
 </React.Fragment>
 
@@ -64,4 +70,4 @@ Music certificate: Real music school
   }
 }
 
-export default App;
+export default About;
