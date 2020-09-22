c4559530dc5f9f83468f904edae2ea3319ef3b98
Irene Kilonzo <irenedunge@yahoo.com>
2020-09-22 10:51:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,84 +1,83 @@
+  
 import React from 'react';
 import './App.css';
 import './W3.css';
-
-const App: React.FC = () => (
+class About extends React.Component {
   render() {
     return (
-      <div className="w3-container" style={{width:"300px"}}>
+      <div className="w3-container ">
         <React.Fragment>
        <h1 className="w3-center">
          About me</h1>
+         <img className="w3-image w3-center" src="designer_.svg" alt="img"></img>
 
-        <h3>Irene Kilonzo</h3>
-              <h5>Front-end developer</h5>
+        <h3 className="w3-center">Irene Kilonzo</h3>
+              <h5 className="w3-center">Front-end developer</h5>
 
-Creative, visionary and responsible frontend developer with 5+ years’ experience in the IT domain. Self-taught and extremely knowledgeable in various web development technologies. Fast learner and flexible.
+<p className="w3-text w3-center">I am a Creative, visionary and responsible frontend developer with 5+ years’ experience in the IT domain. Self-taught and extremely knowledgeable of various web development technologies. Fast learner and flexible.
+</p>
+<h3 className="w3-text w3-center">TECHNICAL SKILLS</h3>
+<img className="w3-image w3-center" src="code.svg" alt="img"></img>
 
-<h3>TECHNICAL SKILLS</h3>
-<img className="logo w3-center" src="coworking_.svg" alt="img"></img>
-<ul>
-  <li>
+<ul className="w3-ul">
+  <li className="w3-list">
   Create-react-app, R.T.L, jest, custom react set-ups, react-redux, github, API intergration</li>
 
 
 
 
 
-<li>Strong experience in working with w3-CSS Backgrounds, w3-CSS Layouts, w3-CSS Positioning, w3-CSS Animations, w3-CSS Text, w3-CSS Borders, w3-CSS Margin, w3-CSS Padding, w3-CSS Table and Pseudo Classes.
+<li className="w3-list w3-center">Strong experience in working with w3-CSS Backgrounds, w3-CSS Layouts, w3-CSS Positioning, w3-CSS Animations, w3-CSS Text, w3-CSS Borders, w3-CSS Margin, w3-CSS Padding, w3-CSS Table and Pseudo Classes.
 </li>
 
- <li>Experience on working with CSS Backgrounds, CSS Layouts, CSS Positioning, CSS Animations, CSS Text, CSS Borders, CSS Margin, CSS Padding, CSS Table and Pseudo Classes.</li>
+ <li className="w3-list w3-center">Experience on working with CSS Backgrounds, CSS Layouts, CSS Positioning, CSS Animations, CSS Text, CSS Borders, CSS Margin, CSS Padding, CSS Table and Pseudo Classes.</li>
 
 <li>Expertise in Client Side designing and validations using HTML, JSX and Java Script.</li>
 
-<li>Thorough researcher and learner.</li>
+<li className="w3-list w3-center">Thorough researcher and learner.</li>
 </ul>
 
-<h3>EXPERIENCE</h3>
-<img className ="w3-image w3-center" src="coding_.svg" alt ="img"></img>
-<ul>
-  <h4>2018-present Front-end development;</h4>
+<h3 className="w3-center">EXPERIENCE</h3>
+<img className="w3-image w3-center" src="coworking.svg" alt="img"></img>
+
+<ul className="w3-ul">
+  <li className="w3-list">2018-present:<b>Front-end development</b> 
 
 
-<li> Using react.JS, JavaScript, HTML, and CSS for developing single page application and achieving two-way data binding between HTML and JavaScript objects.</li>
+Using react.JS, JavaScript, HTML, and CSS for developing single page application and achieving two-way data binding between HTML and JavaScript objects.
 
-<li>Using JavaScript, Dom manipulation and JavaScript event to generate the data result in UI.</li>
+Using JavaScript, Dom manipulation and JavaScript event to generate the data result in UI.
 
-<li>API integrations and implementation in the front end using axios and fetch</li>
-     <li> Using React-Router to turn application into Single Page Applications.</li>
+API integrations and implementation in the front end using axios and fetch</li>
+      Using React-Router to turn application into Single Page Application
 
 
-<h4>2013-2018 Muthusi creatives</h4>
+<li>2013-2018:<b> Muthusi creatives</b>
 
-<li> Helped build client websites with wordpress</li>
-<li>Modified existing code as required. </li>
-<li> Assisted in website traffic and performance monitoring. </li>
-
-      </ul>
-
+Helped build client websites with wordpress</li>
+</ul>
 
 
 
 
 
-<h3>EDUCATION</h3>
-<img className="w3-image w3-center" src="focused_working.svg" alt ="img"></img>
+<h3 className="w3-center">EDUCATION</h3>
 
-      Html, Css, sass, Jsx, javascript, Reactjs, React native : Self Taught.
-<h3>LANGUAGES.</h3>
-<p>Swahili.</p>
-<p>English.</p>
-      <h3>CONTACTS</h3>
-      <p>Email: kilonzohosting@gmail.com</p>
-      <p>Mobile: +254 10 226 1491</p>
-     
+     I.T history, Html, css, w3css, bootstrap, javascript, jsx, React, react-redux, github: Self taught
 
+     <h3>Contacts</h3>
+
+     mobile : 254 10 226 1491
+     Email : kilonzohosting@gmail.com
+   
 </React.Fragment>
+
+
+
 
       </div>
     );
   }
 }
 
-export default App;
+export default About;
